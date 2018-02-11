645dabe304e3e1154cccde88695d5304ac1a99bf
Adam Oakes <amot92@mail.missouri.edu>
2018-02-11 15:49:08 -0600

    Initial Commit

---
+++ GraphicsAssignment1/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  GraphicsAssignment1
+//
+//  Created by Adam Oakes on 2/11/18.
+//  Copyright © 2018 Adam Oakes. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
